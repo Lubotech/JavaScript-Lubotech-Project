7c2e80517b224533f690769452142db02d91a2df
Lubotech <luboyerajoel573@gmail.com>
2024-08-11 04:40:20 +0300

    Saved data and generated HTML for delivery options

--- data/cart.js
+++ data/cart.js
@@ -5,12 +5,15 @@ if (!cart) {
     {
       productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
       quantity: 2,
+      deliveryOptionId:'1'
     }, {
       productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
       quantity: 1,
+      deliveryOptionId:'2'
      }, {
       productId: '54e0eccd-8f36-462b-b68a-8182611d9add',
       quantity: 3,
+      deliveryOptionId:'3'
      }
   ];
 }
@@ -37,7 +40,8 @@ export function addToCart(productId) {
     // If the product is not in the cart, adding it.
     cart.push({
       productId: productId,
-      quantity: 1
+      quantity: 1,
+      deliveryOptionId:'1'
     });
     }
 
