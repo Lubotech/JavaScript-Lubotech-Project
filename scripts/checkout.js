55c4e16aae2ffb745e9e72b5307a009b20ce91fb
Lubotech <luboyerajoel573@gmail.com>
2024-08-30 11:56:19 +0300

    Learnt promises and converted code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,16 +2,64 @@ import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import { renderOrderSummary, } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 // import '../data/cart-class.js';
 //import '../data/backend-practice.js'
 
+Promise.all([
+  new Promise((resolve) =>{
+  
+    loadProducts(() => {
+      resolve('value1');
+    });
+  }),
+  new Promise((resolve) =>{
+    loadCart(() => {
+      resolve();
+    });
+  })
+]).then((values) => {
+  console.log(values);
+  renderCheckoutHeader();
+  renderOrderSummary();
+  renderPaymentSummary();
+});
+
+
+/*
+new Promise((resolve) =>{
+  
+  loadProducts(() => {
+    resolve('value1');
+  });
+}).then((value) => {
+  console.log(value);
+
+  return new Promise((resolve) =>{
+    loadCart(() => {
+      resolve();
+    });
+  });
+}).then(() => {
+  renderCheckoutHeader();
+  renderOrderSummary();
+  renderPaymentSummary();
+  
+});
+
+
+/*
 loadProducts(() => {
   // console.log(products); // You can now use the loaded products array in your app.
   // Start your app here. For example, render the checkout header, order summary, and payment summary.
   // For example:
-   renderCheckoutHeader();
-   renderOrderSummary();
-   renderPaymentSummary();
+
+   loadCart(() => {
+    renderCheckoutHeader();
+    renderOrderSummary();
+    renderPaymentSummary();
+   });
+
   //
   // You can also add event listeners for any buttons or form inputs to update the cart or make requests to the backend.
   // For example:
@@ -26,3 +74,4 @@ loadProducts(() => {
   // const deliveryOptionSelect = document.querySelector('.js-delivery-option-select');
   //
 });
+*/
